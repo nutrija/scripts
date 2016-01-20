50c88d3246893fb66fe89ec848073ab1c4245583
Viktor Jagusic <viktor.jagusic@gmail.com>
2016-01-20 16:53:48 +0100

    skype-notify tweaked

--- skype-notify/notify.py
+++ skype-notify/notify.py
@@ -1,18 +1,41 @@
-# Desktop notification for Skype unread messages
+# report Skype unread messages with notify-send
 
 import sqlite3
 import subprocess
 import argparse
-import threading
+import time
+import signal
+import sys
+import glob
+import os
 
 parser = argparse.ArgumentParser(description='Desktop notifications for Skype unread messages')
-parser.add_argument('database', help='Skype database file (~/.Skype/[account]/main.db)')
-parser.add_argument('-t', type=int, default=15, help='Check interval in seconds')
+parser.add_argument('-d', help='Skype database file (~/.Skype/[account]/main.db)')
+parser.add_argument('-t', type=int, default=2, help='Check interval in seconds')
 args = parser.parse_args()
 
-def skype_notify():
-    threading.Timer(args.t, skype_notify).start()
-    db = sqlite3.connect(args.database)
+if args.d:
+    if (not os.path.exists(args.d)):
+        print "Skype database file not found in " + args.d
+        exit(1)
+    db_path = args.d
+else:
+    db_paths = glob.glob("/home/*/.Skype/*/main.db")
+    if len(db_paths) == 0:
+        print "Skype database file not found (in /home/*/.Skype/*/main.db)"
+        exit(1)
+    db_path = db_paths[0]
+
+print "Listening on " + db_paths[0] + " (Ctrl+C for exit)"
+
+db = sqlite3.connect(db_path)
+
+def signal_handler(signal, frame):
+    db.close()
+    sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
+while True:
     res = db.execute("SELECT COUNT(*) FROM messages WHERE consumption_status=2")
     count = res.fetchone()[0]
     if (count > 0):
@@ -21,6 +44,4 @@ def skype_notify():
         for row in res:
             msg = msg + "{0}: {1}\n".format(row[1], row[0])
         subprocess.call(['notify-send', "Skype: {0} unread messages\n".format(count), msg])
-    db.close()
-
-skype_notify()
+    time.sleep(args.t)
